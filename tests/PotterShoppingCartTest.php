240f523bf9f7af2623ed1aa711f6663dc556cec6
tinymao <winson58kimo@gmail.com>
2016-03-14 11:44:04 +0800

    Scenario:  buy a set of Harry

--- tests/PotterShoppingCartTest.php
+++ tests/PotterShoppingCartTest.php
@@ -96,4 +96,30 @@ class PotterShoppingCartTest extends PHPUnit_Framework_TestCase
         $expected = 320;
         $this->assertEquals($expected, $result);
     }
+
+    /**
+     * Scenario: 一次買了整套，一二三四五集各買了一本，價格應為100*5*0.75=375
+     */
+    public function test_buy_a_set_of_Harry_Potter()
+    {
+        // arrange
+        $books = [
+            new Book(1, "哈利波特第一集", 100),
+            new Book(2, "哈利波特第二集", 100),
+            new Book(3, "哈利波特第三集", 100),
+            new Book(4, "哈利波特第四集", 100),
+            new Book(5, "哈利波特第五集", 100),
+        ];
+
+        $cart = new PotterShoppingCart();
+        $cart->addBooksToCart($books);
+
+        //act
+        $cart->calculate();
+        $result = $cart->total;
+
+        //assert
+        $expected = 375;
+        $this->assertEquals($expected, $result);
+    }
 }
