ec23f4aada127d280e1e2ce36de8a05ea8eae52f
tinymao <winson58kimo@gmail.com>
2016-03-14 14:41:18 +0800

    cart for general

---
+++ src/ShoppingCart.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+
+class ShoppingCart implements Cart
+{
+    public $books = [];
+    public $total = 0;
+
+    /**
+     * 加書到購物車
+     * @param $books
+     */
+    public function addBooksToCart($books)
+    {
+        $this->books = array_merge($this->books, $books);
+    }
+
+    /**
+     * 計算購物車總和
+     *
+     */
+    public function calculate()
+    {
+        $total = 0;
+        foreach ($this->books as $book) {
+            $total += $book->price;
+        }
+
+        $this->total = $total;
+    }
+}
